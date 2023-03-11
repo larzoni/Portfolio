1331462092c376e1b50479a5c236313932096c23
Helena <helena@sixfeetcreative.com>
2023-03-11 23:21:49 +0100

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+    </>
+  );
+};
+
+export default About;
